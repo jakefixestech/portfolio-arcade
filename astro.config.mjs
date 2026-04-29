1f053ef9797a7b7e6f943c0d3f906ef39285cf71
Jake <jake@jakefixestech.com>
2026-04-29 09:06:27 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
+  output: 'server',
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
